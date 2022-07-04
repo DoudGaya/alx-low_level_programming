1f08e08e7e0ee97b799084284880fdcfe0b77528
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-03 18:05:22 -0700

    h

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+
+ * Puzzle docs
+
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle\n");
+return(0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+
+ * this might work docs
+
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
