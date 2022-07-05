4a88b7b7d3ceb5d7852e984c1144551ac49d7b8a
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-05 15:15:26 -0700

    ss

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+// A function that prints putchar as a string
+
+// using only putchar
+
+//then returns 0
+
+int main(void)
+
+{
+_putchar('_');
+_putchar('P');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+ return (0)
+}
