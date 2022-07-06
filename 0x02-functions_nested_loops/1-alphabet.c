2d5d512c26bf59be42f2527058fac732686515f2
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-06 16:20:28 -0700

    ccd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - functions desc
+* Return: returns 0 on evertime
+*/
+
+int print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z' i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+return (0);
+}
