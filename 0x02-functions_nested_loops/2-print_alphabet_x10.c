28fd61a3dfc2d18c03f4fb41c04daac79f6521f9
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-06 18:17:55 -0700

    ccc

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,12 @@ void print_alphabet_x10(void)
 {
 int i;
 int j;
-for (i = 1; i <= 11; i++)
+for (i = 1; i < 11; i++)
 {
 for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
- _putchar('\n');
+_putchar('\n');
 }
 }
