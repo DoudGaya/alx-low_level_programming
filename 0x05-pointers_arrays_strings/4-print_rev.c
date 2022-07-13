5ee82dad993fb139363e9a4976ec9574ac0cc925
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-13 12:33:15 -0700

    something g

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,13 @@ void print_rev(char *s)
 {
 int i = 0;
 int len;
-for (len = 0; s[len] != '\0'; len++)   
+while (s[i] != '\0')
 {
-len++;
-}
-while (i < j)
-{
-char text = s[i];
-s[i] = s[j];
-s[j] = text;
 i++;
-j--;
 }
+for (len = i - 1; len >= 0; len--)
+{
+_putchar(s[len]);
+}
+_putchar('\n');
 }
