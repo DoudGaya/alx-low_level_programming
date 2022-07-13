334502da43edfa5883801ff59eea966c215006c0
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-13 16:03:29 -0700

    kop

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* puts2 - Prints a string
+* @str: String to print
+* Return: Nothing
+*/
+void puts2(char *str)
+{
+int i = 0;
+int len;
+len = _strlen(str);
+for (i = 0; i < len; i++)
+{
+if (i % 2 == 0)
+}
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
