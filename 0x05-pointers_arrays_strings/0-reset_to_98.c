ce2138dfbef8b9071c567a68612f2fd457e31b56
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-07-13 01:28:04 -0700

    sd

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,13 +2,12 @@
 #include <main.h>
 
 /**
-* reset_to_string - function that reset address to 98
+* reset_to_98- function that reset address to 98
 * @n: parameter of the function
-*
+*Return: return 98
 */
 
 void reset_to_98(int *n)
-  
 {
-  *n = 98; 
+*n = 98;
 }
