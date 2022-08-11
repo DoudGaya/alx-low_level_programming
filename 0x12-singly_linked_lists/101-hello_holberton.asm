fadb20b8029ea70e7c42e0f4458d8752d5607e65
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-08-11 14:22:33 -0700

    s

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,4 +1,4 @@
-ECTION .data
+	SECTION .data
 
 msg:	db "Hello, Holberton", 0
 
@@ -7,17 +7,25 @@ fmt:	db "%s", 10, 0
 
 
 	SECTION .text
+
 	extern printf
+
 	global main
 
 main:
 
 	mov esi, msg
+
 	mov edi, fmt
+
 	mov eax, 0
+
 	call printf
+
+
 
 	mov eax, 0
 
 	ret
+
 
