ff17a95690e392461b89a0c3b9dc5d4216a733ba
Abdulrahman Dauda <arbdoule@yahoo.com>
2022-08-15 23:19:05 -0700

    saa

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,18 +2,30 @@
 
 /**
  * get_nodeint_at_index - nth node of a linked list
+ *
  * @head: params head of a list.
+ *
  * @index: index of list
+ *
  * Return: head of a list.
+ *
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+
 {
+
 	int i;
 
 	for (i = 0; i < index && head != NULL; i++)
+
 	{
+
 		head = head->next;
+
 	}
+
 	return (head);
+
 }
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -5,8 +5,7 @@
  * @head: params head
  * @idx: index of the list
  * @n: integer element.
- * Return: the address of the new node, or NULL if it
- * failed.
+ * Return: the address of the new node, or NULL
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
